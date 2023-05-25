bb5e259465974a764879dc45587ee205089a8f9e
FelixOtienoArogo <felixarogo@gmail.com>
2023-05-25 22:21:59 +0300

    he

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python3
 """
-Module for handling Personal Data
+Module filter_datum
 """
 from typing import List
 import re
 
 
-def filter_datum(fields: List[str], redaction: str,
-                 message: str, separator: str) -> str:
-    """ Returns a log message obfuscated """
+def filter_datum(fields: List[str], redaction: str, message: str,
+                 separator: str) -> str:
+    """ Return the log message obfuscated. """
     for f in fields:
         out = re.sub(f'{f}=.*?{separator}',
-                         f'{f}={redaction}{separator}', message)
-    return out
+                     f'{f}={redaction}{separator}', message)
+    return message
