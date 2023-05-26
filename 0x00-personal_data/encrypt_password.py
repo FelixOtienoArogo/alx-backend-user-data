510ec000564721c5cea1e6cb15de5da4343d100c
FelixOtienoArogo <felixarogo@gmail.com>
2023-05-26 14:56:24 +0300

    Check valid password

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -7,3 +7,11 @@ def hash_password(password: str) -> bytes:
     """Return a hashed password."""
     encoded = password.encode()
     return bcrypt.hashpw(encoded, bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """Validate that the provided password matches the hashed password."""
+    encoded = password.encode()
+    if bcrypt.checkpw(encoded, hashed_password):
+        return True
+    return False
