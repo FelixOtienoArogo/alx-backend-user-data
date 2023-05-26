352ae8c3c2d357916075d706783913162ead59ae
FelixOtienoArogo <felixarogo@gmail.com>
2023-05-26 10:00:02 +0300

    Regex-ing

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,33 +1,13 @@
 #!/usr/bin/env python3
-"""filter_datum."""
+"""Module for data logging."""
 from typing import List
 import re
-import logging
 
 
-def filter_datum(fields: List[str], redaction: str, message: str,
-                 separator: str) -> str:
+def filter_datum(fields: List[str], redaction: str,
+                 message: str, separator: str) -> str:
     """Return the log message obfuscated."""
-    for f in fields:
-        out = re.sub(f'{f}=.*?{separator}',
-                     f'{f}={redaction}{separator}', message)
+    for field in fields:
+        message = re.sub(f'{field}=.*?{separator}',
+                         f'{field}={redaction}{separator}', message)
     return message
-
-
-class RedactingFormatter(logging.Formatter):
-    """Redacting Formatter class."""
-
-    REDACTION = "***"
-    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
-    SEPARATOR = ";"
-
-    def __init__(self, fields: List[str]):
-        """Initialise class."""
-        super(RedactingFormatter, self).__init__(self.FORMAT)
-        self.fields = fields
-
-    def format(self, record: logging.LogRecord) -> str:
-        """Filter values in incoming log records using filter_datum."""
-        record.msg = filter_datum(self.fields, self.REDACTION,
-                                  record.getMessage(), self.SEPARATOR)
-        return super(RedactingFormatter, self).format(record)
