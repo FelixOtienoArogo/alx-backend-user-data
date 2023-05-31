0fc12c2f077db21d01653b286d281f5d40f86377
FelixOtienoArogo <felixarogo@gmail.com>
2023-05-31 20:25:38 +0300

    Create a session

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -1,11 +1,22 @@
 #!/usr/bin/env python3
 """Auth."""
 from flask import request
-from typing import List, TypeVar
+from typing import List, TypeVar, Dict
 from api.v1.auth.auth import Auth
 from base64 import b64decode
 from models.user import User
+import uuid
 
 
 class SessionAuth(Auth):
     """Manage the API authentication by Session Authentication."""
+
+    user_id_by_session_id: Dict[str, str] = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """Create a Session ID for a user_id."""
+        if user_id is None or not isinstance(user_id, str):
+            return None
+        session_id = str(uuid.uuid4())
+        self.user_id_by_session_id[session_id] = user_id
+        return session_id
