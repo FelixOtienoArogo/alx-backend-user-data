b1c76ae89c2a6f882eeab5bc6c895d5936c8cd43
FelixOtienoArogo <felixarogo@gmail.com>
2023-05-31 20:51:58 +0300

    User ID for Session ID

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """Auth."""
-from flask import request
+from flask.globals import session
 from typing import List, TypeVar, Dict
 from api.v1.auth.auth import Auth
 from base64 import b64decode
@@ -20,3 +20,9 @@ class SessionAuth(Auth):
         session_id = str(uuid.uuid4())
         self.user_id_by_session_id[session_id] = user_id
         return session_id
+
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        """Return a User ID based on a Session ID."""
+        if session_id is None or not isinstance(session_id, str):
+            return None
+        return self.user_id_by_session_id.get(session_id, None)
