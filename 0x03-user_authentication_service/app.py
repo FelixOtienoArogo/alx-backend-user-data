a8d83fd8e63456b2484e8365e729f6b6ffbcc117
FelixOtienoArogo <felixarogo@gmail.com>
2023-06-06 15:27:33 +0300

    Log in

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -27,5 +27,22 @@ def users() -> str:
         return jsonify({"message": "email already registered"}), 400
 
 
+@app.route('/sessions', methods=['POST'])
+def login():
+    """Respond to the POST /sessions route."""
+    try:
+        email = request.form['email']
+        password = request.form['password']
+    except KeyError:
+        abort(400)
+    if not AUTH.valid_login(email, password):
+        abort(401)
+    session_id = AUTH.create_session(email)
+
+    message = jsonify({"email": email, "message": "logged in"})
+    message.set_cookie("session_id", session_id)
+    return message
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
